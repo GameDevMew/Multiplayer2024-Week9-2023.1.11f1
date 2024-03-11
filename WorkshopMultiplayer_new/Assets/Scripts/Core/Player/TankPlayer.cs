51eabc7a191e4afe1678c81b8336c8461e324dc5
iGxth <thitipong.hans@bumail.net>
2024-03-11 12:26:34 +0700

    gg

--- WorkshopMultiplayer_new/Assets/Scripts/Core/Player/TankPlayer.cs
+++ WorkshopMultiplayer_new/Assets/Scripts/Core/Player/TankPlayer.cs
@@ -4,6 +4,8 @@ using Unity.Netcode;
 using UnityEngine;
 using Cinemachine;
 using Unity.Collections;
+using System;
+using Unity.VisualScripting;
 
 public class TankPlayer : NetworkBehaviour
 
@@ -12,11 +14,17 @@ public class TankPlayer : NetworkBehaviour
     [Header("References")]
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
 
+    [field:SerializeField] public Health Health {  get; private set; }
+
     [Header("Settings")]
     [SerializeField] private int ownerPriority = 15;
 
     public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
     public NetworkVariable<int> PlayerColorIndex = new NetworkVariable<int>();
+
+    public static event Action<TankPlayer> OnPlayerSpawned;
+    public static event Action<TankPlayer> OnPlayerDespawned;
+        
 
     public override void OnNetworkSpawn()
     {
@@ -28,6 +36,8 @@ public class TankPlayer : NetworkBehaviour
 
             PlayerName.Value = userData.userName;
             PlayerColorIndex.Value = userData.userColorIndex;
+
+            OnPlayerSpawned?.Invoke(this);
         }
 
         if(IsOwner)
@@ -35,4 +45,21 @@ public class TankPlayer : NetworkBehaviour
             virtualCamera.Priority = ownerPriority;
         }
     }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsServer)
+        {
+            OnPlayerDespawned?.Invoke(this);
+        }
+    }
+
+    
+
+
+
+
+
+
+
 }
